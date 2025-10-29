d874deea73f5b360f4e51fbe3d5f98054d6498de
Mike Miller <millms302@gmail.com>
2025-10-28 21:43:44 -0500

    version 2

--- scripts/app1.js
+++ scripts/app1.js
@@ -1,2 +1,20 @@
-// DOM = Document Object Model
-//
+//this is a change made in app.js file
+function readContent(){
+let title = $("#txtTitle").val();
+let desc = $("#txtDescription").val();
+let color = $("#selColor").val();
+let date = $("#selStatus").val();
+let status = $("#selStatus").val();
+let budget = $("#numBudget").val();
+console.log(title, desc, color, date, status, budget)
+}
+
+
+
+function init(){
+    console.log("App Initialized");
+    $("#btnSave").click(readContent);
+}
+
+
+window.onload = init;
